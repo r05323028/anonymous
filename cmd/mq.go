6295d2391767f63574245c88e285e9b15ad6dbf2
SeanChang <seanchang@Sean-Macbook-Air.local>
2022-01-04 21:50:01 +0800

    Add mq cmd, module

--- cmd/mq.go
+++ cmd/mq.go
@@ -1,6 +1,12 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"log"
+
+	"github.com/nsqio/go-nsq"
+	"github.com/r05323028/anonymous/mq"
+	"github.com/spf13/cobra"
+)
 
 var mqCmd = &cobra.Command{
 	Use:   "mq",
@@ -9,6 +15,41 @@ var mqCmd = &cobra.Command{
 	},
 }
 
+var startConsumer = &cobra.Command{
+	Use:   "start-consumer",
+	Short: "Start consumer",
+	Run: func(cmd *cobra.Command, args []string) {
+		mq.RunConsumer(host, port)
+	},
+}
+
+var pushMessage = &cobra.Command{
+	Use:   "push",
+	Short: "Push message",
+	Run: func(cmd *cobra.Command, args []string) {
+		config := nsq.NewConfig()
+		producer, err := nsq.NewProducer("nsqd:4150", config)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		messageBody := []byte("hello")
+		topicName := "anonymous"
+
+		// Synchronously publish a single message to the specified topic.
+		// Messages can also be sent asynchronously and/or in batches.
+		err = producer.Publish(topicName, messageBody)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Gracefully stop the producer when appropriate (e.g. before shutting down the service)
+		producer.Stop()
+	},
+}
+
 func init() {
+	mqCmd.AddCommand(startConsumer)
+	mqCmd.AddCommand(pushMessage)
 	rootCmd.AddCommand(mqCmd)
 }
---
+++ mq/consumer.go
@@ -0,0 +1,47 @@
+package mq
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/nsqio/go-nsq"
+)
+
+type MessageHandler struct{}
+
+func (h *MessageHandler) HandleMessage(m *nsq.Message) error {
+	if len(m.Body) == 0 {
+		// Returning nil will automatically send a FIN command to NSQ to mark the message as processed.
+		// In this case, a message with an empty body is simply ignored/discarded.
+		return nil
+	}
+	log.Println(string(m.Body))
+	err := errors.New("Test")
+	return err
+}
+
+func RunConsumer(host string, port string) {
+	config := nsq.NewConfig()
+	consumer, err := nsq.NewConsumer("anonymous", "general", config)
+	if err != nil {
+		log.Fatal(err)
+	}
+	consumer.AddHandler(&MessageHandler{})
+	dsn := fmt.Sprintf("%v:%v", "nsqlookupd", "4161")
+	err = consumer.ConnectToNSQLookupd(dsn)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// wait for signal to exit
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+	<-sigChan
+
+	// Gracefully stop the consumer.
+	consumer.Stop()
+}
